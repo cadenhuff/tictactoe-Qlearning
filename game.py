c97f0aa59e0f54b465147678a0c9a916aef34014
Caden Huffman <cadenhuf@bu.edu>
2024-01-01 17:25:54 -0500

    Creating TicTacToe class

---
+++ game.py
@@ -0,0 +1,36 @@
+from enum import Enum
+
+
+
+class TicTacToe:
+
+    def __init__(self):
+        self.board = []
+
+        self.turn = "X"
+
+
+
+
+    def update_board(self):
+
+        pass
+
+
+    def play_game(self):
+        #Display Board
+
+        #Player picks spot
+
+        #Check for winner
+
+
+        return game_over, winner
+        pass
+
+
+    
+
+
+
+if __name__ == 
